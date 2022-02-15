4dd04f92d5002d840867946ff90116631feaaf74
Joao Vitor Lima <joao.lima@devio.com.br>
2022-02-15 20:47:38 -0300

    All tests passed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (!user.admin) {
+      throw new Error("You don't have permission to take this action");
+    }
+
+    const allUsers = this.usersRepository.list();
+
+    return allUsers;
   }
 }
 
